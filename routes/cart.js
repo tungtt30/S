0891b347662d432332dd4c4d44194eab9b5fbdc9
tungtt30 <tungchipsctn2@gmail.com>
2022-07-05 17:06:31 +0700

    asdasd

--- routes/cart.js
+++ routes/cart.js
@@ -1,36 +1,77 @@
 const express = require('express')
 const router = express.Router()
 const verifyToken = require('../middleware/auth')
-
+const User = require('../models/User')
 const Cart = require('../models/Cart')
 
 //get one user
-
 router.get('/', verifyToken, async (req, res) => {
-    const userid = req.params.userid
     try {
-        const cartInfo = await Cart.findOne({ userid }).populate(['item', 'user'], ['username'])
-        res.status(200).json({ message: 'ok', cartInfo, req: req.userId })
-    } catch (error) {
-        res.json({ message: 'bad request' })
+        // const user = await User.findOne({user})
+        const cart = await Cart.find({ user: req.userId })
+        res.status(200).json({ message: 'ok', cart })
+    } catch (e) {
+        res.status(500).json({ message: 'error' })
     }
 })
+
 
 
 // create new item 
-router.post('/', verifyToken, async (req, res) => {
-    const { item, quantity, user } = req.body
-    try {
-        const newItem = new Cart({
-            item, // itemId
-            quantity,
-            user // userId
-        })
-        await newItem.save()
-        res.status(200).json({ message: 'ok', newItem })
 
+router.post('/', verifyToken, async (req, res) => {
+    const { item, quantity } = req.body
+
+    if (!item && !quantity)
+        return res.status(400).json({ success: false, message: "item and quantity required" })
+    try {
+        const newCart = new Cart({
+            item,
+            quantity,
+            user: req.userId
+        })
+
+        await newCart.save()
+
+        res.status(200).json({ message: 'success', newCart })
     } catch (error) {
-        res.json({ message: 'bad request' })
+        res.status(500).json({ message: 'server error' })
     }
 })
+
+router.put('/:cartId', verifyToken, async (req, res) => {
+    const { item, quantity } = req.body
+    try {
+        let updatedCart = {
+            item,
+            quantity,
+            user: req.userId
+        }
+        const cartUpdateCondition = { _id: req.params.id, user: req.userId }
+        updatedCart = await Cart.findOneAndUpdate(cartUpdateCondition, updatedCart, { new: true })
+        if (!updatedCart) {
+            return res.status(401).json({ success: false, message: 'Cart not found or not authorized' })
+        }
+        res.json({ success: true, message: "ok", updatedCart })
+    } catch (error) {
+        res.status(400).json({ message: 'bad request' })
+    }
+})
+
+router.delete('/:cartId', verifyToken, async (req, res) => {
+    try {
+        const cartDeleteCondition = { _id: req.params.id, user: req.userId }
+
+        const deletedCart = await Post.findByIdAndDelete(cartDeleteCondition)
+
+        // user not authorized
+        if (!deletedCart)
+            return res.status(401).json({ success: false, message: 'Cart not found or not authorized' })
+
+        res.json({ success: true, message: "Deleted", deletedCart })
+    } catch (error) {
+        res.status(500).json({ message: 'server error' })
+    }
+})
+
 module.exports = router
